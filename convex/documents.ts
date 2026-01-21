5609295f62f3a607f521a6c3d88ff3604b9c37d2
Tanabe-Yumi <alleyoop58dank@gmail.com>
2026-01-21 10:09:14 +0900

    enabled to create a new document by clicking templete

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,5 @@
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 // データ取得関数 (API エンドポイント)
 // - スキーマが変更されると、get メソッドの戻り値の型もリアルタイムに更新される
@@ -6,5 +7,27 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("documents").collect();
+  },
+});
+
+// ドキュメント作成用メソッド
+// 戻り値: insert data の id
+export const create = mutation({
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+
+    if (!user) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    return await ctx.db.insert("documents", {
+      title: args.title ?? "Untitled document",
+      ownerId: user.subject,
+      initialContent: args.initialContent,
+    });
   },
 });
