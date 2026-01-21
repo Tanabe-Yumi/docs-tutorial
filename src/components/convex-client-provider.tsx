cb5493a537ab537e5c1576b2b437f9f813cca3c2
Tanabe-Yumi <alleyoop58dank@gmail.com>
2026-01-21 09:06:02 +0900

    added fullscreen loading page while auth loading

--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -9,6 +9,7 @@ import {
 } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth, SignIn } from "@clerk/clerk-react";
+import { FullscreenLoader } from "./fullscreen-loader";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -25,7 +26,7 @@ export function ConvexClientProvider({ children }: { children: ReactNode }) {
           </div>
         </Unauthenticated>
         <AuthLoading>
-          <p>Loading auth...</p>
+          <FullscreenLoader label="Auth loading..." />
         </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
