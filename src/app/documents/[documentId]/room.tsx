4733e58754947d1aa339025b7440989cb666c645
Tanabe-Yumi <alleyoop58dank@gmail.com>
2026-01-23 14:54:51 +0900

    fixed to display full-screen loading while authentication

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -7,6 +7,7 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { FullscreenLoader } from "@/components/fullscreen-loader";
 
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
@@ -21,7 +22,9 @@ export function Room({ children }: { children: ReactNode }) {
     >
       {/* roomID = documentID */}
       <RoomProvider id={params.documentId as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense
+          fallback={<FullscreenLoader label="Room loading..." />}
+        >
           {children}
         </ClientSideSuspense>
       </RoomProvider>
